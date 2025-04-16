d3e434dbdaeec9684b7dfc4bf106f5168029dc76
nirajku103 <nirajnetjob.hojai@gmail.com>
2025-04-16 09:26:35 +0000

    changing bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "mock-hackathon-uc01"
-    key            = "terraform"
-    region         = "eu-north-1"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "eu-north-1"
 }
